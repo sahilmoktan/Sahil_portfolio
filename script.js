68509ec2b219c0440baac650872c7a84eedfcbe9
sahilmoktan <moktanshail9@gmail.com>
2023-09-22 21:22:27 +0545

    goto Top button, and miner changes

--- script.js
+++ script.js
@@ -10,8 +10,30 @@ function openInNewTab(url) {
 }
 
 var typed = new Typed(".section_text_p2", {
-  strings: ["JavaScript Developer","NodeJS Developer", "NextJS Developer"],
+  strings: ["JavaScript Developer", "NodeJS Developer", "NextJS Developer"],
   typeSpeed: 80,
   backSpeed: 25,
   loop: true,
 });
+
+// script.js
+window.onscroll = function () {
+  toggleBackToTopButton();
+};
+
+function toggleBackToTopButton() {
+  var button = document.getElementById("backToTopBtn");
+  if (
+    document.documentElement.scrollTop > 100 ||
+    document.body.scrollTop > 100
+  ) {
+    button.style.display = "block";
+  } else {
+    button.style.display = "none";
+  }
+}
+
+function scrollToTop() {
+  document.documentElement.scrollTop = 0;
+  document.body.scrollTop = 0;
+}
