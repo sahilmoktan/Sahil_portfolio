9c3f9dffdb94f1ff78e67eca2342aa6ae2ee90cf
sahilmoktan <moktanshail9@gmail.com>
2023-09-22 19:59:43 +0545

    typing effect, about me update

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 function toggleMenu() {
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
-  }
-  
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
 
-  function openInNewTab(url) {
-    window.open(url, '_blank');
-  }
+function openInNewTab(url) {
+  window.open(url, "_blank");
+}
+
+var typed = new Typed(".section_text_p2", {
+  strings: ["JavaScript Developer","NodeJS Developer", "NextJS Developer"],
+  typeSpeed: 80,
+  backSpeed: 25,
+  loop: true,
+});
